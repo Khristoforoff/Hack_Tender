4443863e2384dad1e17bffd81dbba6e064b9b893
viktor65dav@gmail.com <viktor65dav@gmail.com>
2020-02-09 02:34:24 +0300

    Добавьте файлы проекта.

---
+++ FreshRating/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+
+namespace FreshRating
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /// <summary>
+        /// Самое начало
+        /// </summary>
+        public MainWindow()
+        {
+
+        }
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void BuyerClick(object sender, RoutedEventArgs e)
+        {
+            Window buyer = new Buyer();
+            buyer.Show();
+        }
+        private void SellerClick(object sender, RoutedEventArgs e)
+        {
+            Window seller = new Seller();
+            seller.Show();
+        }
+    }
+}
